f00858c62b37085685f907226e26cbf11182f41f
Carlos Orellana <carlos.cw2106@gmail.com>
2016-10-23 17:05:46 -0600

    Refactoring create and show actions in post controller. Adding the params method and the set_post method to in order to run a before_action method

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,13 +14,16 @@ describe 'navigate' do
   end
 
   describe "Creation" do
+
+    before do
+      visit new_post_path
+    end
+
     it "Has a new form that can be reached" do
-       visit new_post_path
        expect(page.status_code).to eq(200)
     end
 
     it "Can be created from new form page" do
-       visit new_post_path
        fill_in 'post[date]', with: Date.today
        fill_in 'post[rationale]', with: "Some Rationale"
 
